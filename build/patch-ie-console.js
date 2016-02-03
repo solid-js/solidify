46badb03064e63bb6e3fd46b111aef4127ed7a62
Alexis Bouhet <zouloux@Alexiss-MacBook-Pro.local>
2016-02-03 14:58:10 +0100

    IE Console patch is now working correctly

--- build/patch-ie-console.js
+++ build/patch-ie-console.js
@@ -1,8 +1,17 @@
-if ('console' in window)
-{
+(function () {
+	var u = function () {};
+	if (!('console' in window))
+	{
+		window.console = {
+			log: u,
+			warn: u,
+			error: u,
+			table: u
+		};
+	}
 	if (!('group' in console))
 	{
 		console.group = console.log;
-		console.groupEnd = function () {};
+		console.groupEnd = u;
 	}
-}
+})();
