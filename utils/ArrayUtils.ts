d6bfdf35eeac43762ccb857ff41ec37fd7d92cec
Alexis Bouhet <alexis@cher-ami.tv>
2017-02-28 18:38:36 +0100

    Array shuffle utils

--- utils/ArrayUtils.ts
+++ utils/ArrayUtils.ts
@@ -78,4 +78,32 @@ export class ArrayUtils
 		// Return new array
 		return newArray;
 	}
+
+	/**
+	 * Shuffle an indexed array.
+	 * Source : https://bost.ocks.org/mike/shuffle/
+	 * @param pArray The indexed array to shuffle.
+	 * @returns {any} Original instance of array with same elements at other indexes
+	 */
+	static shuffle (pArray:any[]):any[]
+	{
+		let currentIndex = pArray.length;
+		let temporaryValue;
+		let randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex)
+		{
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = pArray[currentIndex];
+			pArray[currentIndex] = pArray[randomIndex];
+			pArray[randomIndex] = temporaryValue;
+		}
+
+		return pArray;
+	}
 }
