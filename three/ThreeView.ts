84b7e2a2c9e636a9cd8b936845391184e32a0c05
Alexis Bouhet <alexis@cher-ami.tv>
2017-07-12 12:52:43 +0200

    Added Three.js view

--- three/ThreeView.ts
+++ three/ThreeView.ts
@@ -1,69 +1,149 @@
-export = ThreeView;
-
-import View = require("../core/View");
-import TimerUtils = require("lib/solidify/utils/TimerUtils");
-import EnvUtils = require("lib/solidify/utils/EnvUtils");
-import Central = require("lib/solidify/helpers/Central");
+import {Signal} from "../helpers/Signal";
+import {TimerUtils} from "../utils/TimerUtils";
+import {EnvUtils} from "../utils/EnvUtils";
+import {ResponsiveManager} from "../helpers/ResponsiveManager";
 
 
-// TODO : problème d'upscale lorsqu'on resize le browser
 
-/**
- * Config objects
- */
-class BackgroundConfig
+export interface IStereoEffect
+{
+	new (rendererer:THREE.Renderer);
+	setEyeSeparation (eyeSep:number) : void;
+	setSize (width:number, height:number) : void;
+	render (scene:THREE.Scene, camera:THREE.Camera) : void;
+}
+
+
+
+export let StereoEffect:IStereoEffect = (THREE as any).StereoEffect;
+
+
+
+export interface IDeviceOrientationControls
+{
+	new (object:THREE.Object3D);
+	connect ():void;
+	disconnect ():void;
+	update ():void;
+	updateAlphaOffsetAngle (pAngle:number):void;
+	dispose ():void;
+}
+
+export let DeviceOrientationControls:IDeviceOrientationControls = (THREE as any).DeviceOrientationControls;
+
+
+
+
+export interface IViewportSize
+{
+	width	:number;
+	height	:number;
+}
+
+
+export interface IThreeObject
+{
+	object3D		:THREE.Object3D;
+	build 	():void;
+	update 	():void;
+}
+
+
+export enum ERenderMode
+{
+	FALLBACK,
+	WEBGL,
+	WEBVR,
+	STEREO
+}
+
+export interface IBackgroundConfig
 {
 	color	:number;
 	alpha	:number;
 }
-class CameraConfig
+export interface ICameraConfig
 {
-	near	:number;
-	far		:number;
-	fov		:any;
+	near		:number;
+	far			:number;
+	fov			:number;
 }
-class FogConfig
+export interface IFogConfig
 {
 	near	:number;
 	far		:number;
 	color	:number;
 }
 
-class ThreeView extends View
+
+export class ThreeView
 {
+	/**
+	 * Main 3D renderer
+	 */
+	protected _renderer				:THREE.Renderer;
+	get renderer ():THREE.Renderer { return this._renderer; }
+
+	/**
+	 * Stereo effect for VR rendering
+	 */
+	protected _stereoEffect			:IStereoEffect;
+	get stereoEffect ():IStereoEffect { return this._stereoEffect; }
+
+	/**
+	 * If WebGL not available and rendering is done with canvas 2DContext
+	 */
+	protected _canvasMode			:boolean;
+	get canvasMode ():boolean { return this._canvasMode; }
+
+	/**
+	 * Canvas dom element
+	 */
+	get domElement ():HTMLCanvasElement { return this._renderer.domElement; }
+
 	/**
 	 * Main 3D camera
 	 */
-	get camera ()			:THREE.Camera { return this._camera; }
-	private _camera			:THREE.Camera;
-
-	/**
-	 * Main 3D renderer
-	 */
-	get canvasMode ()		:boolean { return this._canvasMode; }
-	private _canvasMode		:boolean			= false;
-
-	/**
-	 * Main 3D renderer
-	 */
-	get renderer ()			:THREE.WebGLRenderer { return this._renderer; }
-	private _renderer		:THREE.WebGLRenderer;
+	protected _camera				:THREE.Camera;
+	get camera ():THREE.Camera { return this._camera; }
 
 	/**
 	 * Main 3D scene
 	 */
-	get scene ()			:THREE.Scene { return this._scene; }
-	private _scene			:THREE.Scene;
+	protected _scene				:THREE.Scene;
+	get scene ():THREE.Scene { return this._scene; }
 
 	/**
 	 * Size of the viewport
 	 */
-	get renderSize ()		:number[] { return this._renderSize; }
-	private _renderSize		:number[];
+	get viewPortSize ():IViewportSize { return this._viewPortSize; }
+	protected _viewPortSize			:IViewportSize = {
+		width: 0,
+		height: 0
+	};
+
+	/**
+	 * Signal on each frame
+	 */
+	protected _onEnterFramed  		:Signal						= new Signal();
+	get onEnterFramed ():Signal { return this._onEnterFramed; }
+
+	/**
+	 * Scene clock. Started when rendering is not paused.
+	 */
+	protected _clock				:THREE.Clock;
+	get clock ():THREE.Clock { return this._clock; }
+
+	/**
+	 * Current frame number
+	 */
+	protected _frame											= 0;
+	get frame ():number { return this._frame; }
 
 	/**
 	 * If the engine is paused, no frame are generated.
 	 */
+	protected _paused				:boolean					= true;
 	get paused ():boolean { return this._paused; }
 	set paused (pValue:boolean)
 	{
@@ -72,82 +152,246 @@ class ThreeView extends View
 		{
 			this._paused = pValue;
 
+			// TODO : Attention sur Vive on est à 90 / 120 FPS
+
 			// Update state
-			if (this._paused)
-			{
-				TimerUtils.removeFrameHandler(this.enterFrameHandler);
-			}
-			else
-			{
-				TimerUtils.addFrameHandler(this, this.enterFrameHandler);
-			}
+			(this._paused)
+				? TimerUtils.removeFrameHandler( this.internalEnterFrameHandler )
+				: TimerUtils.addFrameHandler(this, this.internalEnterFrameHandler);
+
+			// Update clock stage
+			// NOT SURE : Does stop reset time ?
+			this._paused
+				? this._clock.stop()
+				: this._clock.start();
 		}
 	}
-	private _paused			:boolean			= true;
-
 
 	/**
-	 * Initialise the Three engine with a camera and an optional fog.
-	 * Will not start the render loop. Call startRender to start the renderLoop.
-	 * ThreeView.threeCanvas need to be attached to the DOM before rendering.
-	 * @param pBackground Background related options. Keep alpha to 1 for better render performances.
-	 * @param pCamera Camera related options. Keep near and far close for better render performances. Default field of view is 70.
-	 * @param pFog Will apply if a fog to the scene. Set to null to disable fog.
-	 * @param pAntialias Smooth the screen if set to true. Can slow down framerate. Default is false.
-	 * @param pAllowCanvas Allow canvas rendering. May be ultra slow on handheld devices.
-	 * @returns true if the renderer is valid (webGL or canvas if allowed).
+	 * If engine has been started with start() method.
 	 */
-	initEngine (
-		pBackground	:BackgroundConfig 	= {color: 0x000000, alpha: 1},
-		pCamera		:CameraConfig 		= {near: 10, far: 10000, fov: 70},
-		pFog		:FogConfig 			= {near: 10, far: 10000, color: 0x000000},
-		pAntialias	:boolean			= false,
-		pAllowCanvas:boolean			= true,
-		pAutoRatio	:boolean			= false
-	):boolean
+	protected _started				:boolean					= false;
+	get started ():boolean { return this._started; }
+
+
+	// ------------------------------------------------------------------------- INIT
+
+	constructor ()
+	{
+		this.afterConstructor();
+	}
+
+
+	// ------------------------------------------------------------------------- MIDDLE WARES
+
+	/**
+	 * MIDDLE WARE
+	 * Right after construction.
+	 * Useful to prepare or load stuff before engine creation
+	 */
+	protected afterConstructor () { }
+
+	/**
+	 * MIDDLE WARE
+	 * After renderer is configured from parent component.
+	 * Useful to override renderer behavior such as shadowMap
+	 */
+	protected afterRendererInit () { }
+
+	/**
+	 * MIDDLE WARE
+	 * After camera is configured from parent component.
+	 * Useful to override camera behavior.
+	 */
+	protected afterCameraInit () {}
+
+	/**
+	 * MIDDLE WARE
+	 * After controls are configured from parent component.
+	 * Useful to override controls behavior.
+	 */
+	protected afterControlsInit () { }
+
+	/**
+	 * MIDDLE WARE
+	 * After scene is configured from parent component.
+	 * This is where you init the 3D scene
+	 */
+	protected afterSceneInit () { }
+
+	/**
+	 * MIDDLE WARE
+	 * After start is called from parent component.
+	 * Useful to start animation / timers after everything is loaded and ready.
+	 */
+	protected afterStart () { }
+
+	/**
+	 * MIDDLE WARE
+	 */
+	protected enterFrameHandler () { }
+
+
+	// ------------------------------------------------------------------------- HELPERS
+
+	/**
+	 * Get abstract renderer. Will return IStereoEffect if Vr Mode enabled.
+	 * @returns {IStereoEffect|THREE.Renderer}
+	 */
+	protected getAbstractRenderer ():IStereoEffect|THREE.Renderer
+	{
+		return (
+			this._stereoEffect != null
+				? this._stereoEffect
+				: this._renderer
+		);
+	}
+
+
+	// ------------------------------------------------------------------------- ENGINE INIT
+
+	/**
+	 *
+	 * @param pRenderMode Render mode to init. @see ERenderMode. Default is WebGL with canvas fallback mode.
+	 * @param pBackground Configure 2D background.
+	 * @param pAutoSize
+	 * @param pAntialias
+	 * @param pPixelRatio
+	 * @param pRendererParams
+	 */
+	initRenderer (
+		pRenderMode		:ERenderMode		= ERenderMode.FALLBACK,
+
+		pBackground		:IBackgroundConfig 	= {color: 0xFFFFFF, alpha: 1},
+
+		pAutoSize		:boolean			= true,
+
+		pAntialias		:boolean|null		= null,
+
+		pPixelRatio		:number|null		= window.devicePixelRatio,
+
+		pRendererParams :any				= {}
+	)
 	{
 		// Check if we are not already initialised
-		if (this._renderer != null) throw new Error("ThreeView.initEngine // Already initialised.");
+		if (this._renderer != null) throw new Error(`ThreeView.initRenderer // Already initialised.`);
 
-		// Try WebGL context
-		if (EnvUtils.getCapabilities().webGL)
+		// If WebGL context is available
+		let hasWebGL = EnvUtils.getCapabilities().webGL;
+
+		// If WebGL is not available and canvas fallback mode is not allowed
+		if (pRenderMode == ERenderMode.WEBGL && !hasWebGL)
 		{
-			this._renderer = new THREE.WebGLRenderer({
-				antialias: pAntialias
-			});
+			throw new Error(`ThreeView.initRenderer // WebGL rendering not available.`);
 		}
 
-		// Try canvas if allowed
-		else if (pAllowCanvas)
+		// Auto-set antialias if parameter is null
+		if (pAntialias == null)
 		{
-			this._canvasMode = true;
-			this._renderer = new THREE['CanvasRenderer']();
+			// We enable antialias if pixel ratio is pretty small
+			pAntialias = (pPixelRatio < 1.5);
 		}
 
-		// Nothing works
-		else return false;
+		// Set renderer params
+		pRendererParams.antialias = pAntialias;
+		pRendererParams.devicePixelRatio = pPixelRatio;
 
-		// Set the background
-		this._renderer.setClearColor(pBackground.color, pBackground.alpha);
+		// Take from background
+		pRendererParams.clearAlpha = pBackground.alpha;
+		pRendererParams.clearColor = pBackground.color;
 
-		// Define pixel ratio
-		if (pAutoRatio)
+		// Create WebGL renderer if available otherwise create CanvasRenderer
+		this._renderer = (
+			hasWebGL
+				? new THREE.WebGLRenderer( pRendererParams )
+				: new THREE.CanvasRenderer( pRendererParams )
+		) as THREE.Renderer;
+
+		// Register if we are in canvas mode
+		this._canvasMode = !hasWebGL;
+
+		// Stereo rendering mode (no real WebVR API)
+		if (pRenderMode == ERenderMode.STEREO)
 		{
-			this._renderer['setPixelRatio'](window.devicePixelRatio);
+			// Create stereo effect
+			this._stereoEffect = new StereoEffect( this._renderer );
 		}
 
-		// Define content dom
-		this.internalContent = $(this._renderer.domElement);
+		// Real WebVR API
+		else if (pRenderMode == ERenderMode.WEBVR)
+		{
+			// TODO : WebVR API
+			throw new Error('TODO');
+		}
+
+		// Create 3D clock and do not start it
+		this._clock = new THREE.Clock( false );
+
+		// Enable auto-size
+		if (pAutoSize)
+		{
+			ResponsiveManager.instance.onWindowSizeChanged.add(this.windowSizeChangedHandler, this);
+		}
+
+		// Call middleWare
+		this.afterRendererInit();
+	}
+
+	/**
+	 *
+	 * @param pCameraConfig
+	 */
+	initCamera (
+		// TODO : Orthographic camera support
+		pCameraConfig	:ICameraConfig 		= {near: 1, far: 10000, fov: 85},
+	)
+	{
+		// Check if init order is ok
+		if (this._renderer == null) throw new Error(`ThreeView.initCamera // Please init renderer before camera`);
+		if (this._scene != null) throw new Error(`ThreeView.initCamera // Please init camera before scene`);
 
 		// Init camera
-		if (typeof pCamera.fov == "number")
-		{
-			this._camera = new THREE.PerspectiveCamera(pCamera.fov, 1, pCamera.near, pCamera.far);
-		}
-		else if (typeof pCamera.fov == "object")
-		{
-			this._camera = new THREE.OrthographicCamera(pCamera.fov[0], pCamera.fov[1], pCamera.fov[2], pCamera.fov[3], pCamera.near, pCamera.far);
-		}
+		this._camera = new THREE.PerspectiveCamera(
+			pCameraConfig.fov, 1,
+			pCameraConfig.near,
+			pCameraConfig.far
+		);
+
+		// TODO : Orthographic camera
+		// BETTER : CombinedCamera ? CinematicCamera ?
+
+		// Call middle ware
+		this.afterCameraInit();
+	}
+
+	/**
+	 * Init controls.
+	 * BETTER : Default VR controls here ?
+	 * BETTER : Any default controls here ? Maybe too specific to go in this class.
+	 */
+	initControls ()
+	{
+		// Check if init order is ok
+		if (this._renderer == null) throw new Error(`ThreeView.initControls // Please init renderer before controls`);
+		if (this._scene != null) throw new Error(`ThreeView.initControls // Please init controls before scene`);
+
+		// Call middle ware
+		this.afterControlsInit();
+	}
+
+	/**
+	 * Init scene.
+	 * BETTER : Parameters to set XYZ order ?
+	 * @param pFog
+	 */
+	initScene (
+
+		pFog			:IFogConfig 		= {near: 1, far: 4000, color: 0xFFFFFF}
+	)
+	{
+		// Check if init order is ok
+		if (this._renderer == null) throw new Error(`ThreeView.initScene // Please init renderer before scene`);
+		if (this._scene != null) throw new Error(`ThreeView.initScene // Scene already init.`);
 
 		// Init scene
 		this._scene = new THREE.Scene();
@@ -155,6 +399,8 @@ class ThreeView extends View
 		// Apply fog if needed
 		if (pFog != null)
 		{
+			// Set scene background and fog
+			this._scene.background = new THREE.Color( pFog.color );
 			this._scene.fog = new THREE.Fog(
 				pFog.color,
 				pFog.near,
@@ -162,137 +408,120 @@ class ThreeView extends View
 			);
 		}
 
-		// Init stuff from child class
-		this.initExternals();
-		this.initMaterials();
-		this.initScene();
-
-		// Listen when app is resized
-		Central.add("app/resize", this, this.appResizedHandler);
-
-		// Our engine is ok
-		return true;
+		// Call middleWare
+		this.afterSceneInit();
 	}
 
+
+	// ------------------------------------------------------------------------- START
+
 	/**
-	 * Start rendering engine
+	 * Start scene and rendering.
+	 * @param pParentDomElement DOM element containing canvas.
 	 */
-	startEngine ():void
+	start (pParentDomElement:HTMLElement)
 	{
-		// Check if we are on DOM
-		if (!this.isAttachedToDocument)
-		{
-			//throw new Error("ThreeView.startRender // ThreeView.threeCanvas need to be attached to DOM before the rendering can start.");
-		}
+		// Check if renderer is init
+		if (this._renderer == null) throw new Error(`ThreeView.initScene // Please init renderer before starting 3D view.`);
 
-		// Resize
-		this.updateRenderSize();
+		// BETTER : Check parent dom element not null
 
-		// We got dimensions
-		this.engineStarted();
+		// Append to parent
+		pParentDomElement.appendChild( this.domElement );
 
-		// Enable render loop
+		// Updates sizes if parent is available
+		this.updateRenderingSizeFromParent();
+
+		// Call middleWare now we have correct sizes
+		this.afterStart();
+
+		// Start loop
 		this.paused = false;
 
-		this.enterFrameHandler();
+		// Remember we started the game
+		this._started = true;
 	}
 
-	/**
-	 * We got dimensions
-	 */
-	engineStarted ():void {}
 
-	/**
-	 * Init external assets (abstract method)
-	 */
-	initExternals ():void {}
-
-	/**
-	 * Init materials (abstract method)
-	 */
-	initMaterials ():void {}
-
-	/**
-	 * Init scene (abstract method)
-	 */
-	initScene ():void {}
-
-	/**
-	 * Update render size from domElement
-	 */
-	updateRenderSize ():void
-	{
-		// Target parent
-		var parent = this.content.parent();
-
-		// No dimensions if not attached to root
-		if (parent == null)
-		{
-			this._renderSize = [0, 0];
-		}
-
-		// Get dimensions from parent
-		else if (this._renderer != null)
-		{
-			this._renderSize = [parent.width(), parent.height()];
-
-			// Update camera aspect ratio
-			if ("aspect" in this._camera)
-			{
-				(<THREE.PerspectiveCamera>this._camera).aspect = this._renderSize[0] / this._renderSize[1];
-			}
-			else
-			{
-				(<THREE.OrthographicCamera>this._camera).left = -this._renderSize[0] / 2;
-				(<THREE.OrthographicCamera>this._camera).right = this._renderSize[0] / 2;
-				(<THREE.OrthographicCamera>this._camera).top = this._renderSize[1] / 2;
-				(<THREE.OrthographicCamera>this._camera).bottom = -this._renderSize[1] / 2;
-			}
-
-			// Update camera projection
-			if ("updateProjectionMatrix" in this._camera)
-			{
-				(<THREE.PerspectiveCamera>this._camera).updateProjectionMatrix();
-			}
-
-			// Update renderer
-			this._renderer.setSize(this._renderSize[0], this._renderSize[1]);
-		}
-	}
+	// ------------------------------------------------------------------------- SIZE
 
 	/**
 	 * App is resized.
-	 * NB : Ici on fait une closure pour ne pas avoir de problème lors du Central.remove
 	 */
-	private appResizedHandler = ():void =>
+	protected windowSizeChangedHandler ()
 	{
-		//console.log("ThreeView.appResizedHandler", this.viewName);
-
 		// Get dimensions from node
-		this.updateRenderSize();
+		this.updateRenderingSizeFromParent();
 	};
 
 	/**
-	 * Called on each frames.
-	 * NB : Ici on fait une closure pour ne pas avoir de problème lors du removeFrameHandler
+	 * Set rendering size from domElement size.
+	 * NOT SURE : VR Compatible ?
+	 * BETTER : Détecter ou informer si parent non disponible
 	 */
-	enterFrameHandler = ():void =>
+	updateRenderingSizeFromParent ()
 	{
-		this._renderer.render(this._scene, this._camera);
-	};
+		// Target parent dom node
+		let parent = this._renderer.domElement.parentElement;
 
-	/**
-	 * Destruction
-	 */
-	dispose ():void
+		// Get size from parent
+		if (parent != null)
+		{
+			this._viewPortSize.width = parent.clientWidth;
+			this._viewPortSize.height = parent.clientHeight;
+		}
+
+		// Or fallback to 0
+		// BETTER : Dispatch error ? (Be careful with autoSize)
+		else
+		{
+			this._viewPortSize.width = 0;
+			this._viewPortSize.height = 0;
+		}
+
+		// Update camera aspect ratio
+		if ('aspect' in this._camera)
+		{
+			(this._camera as THREE.PerspectiveCamera).aspect = (
+				this._viewPortSize.width / this._viewPortSize.height
+			);
+		}
+		else if ('left' in this._camera)
+		{
+			// BETTER : Test this
+			let orthographicCamera = (this._camera as THREE.OrthographicCamera);
+			orthographicCamera.left 	= - this._viewPortSize.width / 2;
+			orthographicCamera.right 	= + this._viewPortSize.width / 2;
+			orthographicCamera.top 		= + this._viewPortSize.height / 2;
+			orthographicCamera.bottom 	= - this._viewPortSize.height / 2;
+		}
+
+		// Update camera projection
+		if ('updateProjectionMatrix' in this._camera)
+		{
+			(this._camera as THREE.PerspectiveCamera).updateProjectionMatrix();
+		}
+
+		// Update renderer size
+		this.getAbstractRenderer().setSize(
+			this._viewPortSize.width,
+			this._viewPortSize.height
+		);
+	}
+
+
+	// ------------------------------------------------------------------------- LOOP
+
+
+	protected internalEnterFrameHandler ()
 	{
-		// Delete app resize listening
-		Central.remove("app/resize", this.appResizedHandler);
+		this.enterFrameHandler();
+		this._frame ++;
+		this.render();
+	}
 
-		// Stop the timer
-		TimerUtils.removeFrameHandler(this.enterFrameHandler);
-
-		// Relay
-		super.dispose();
+	render ()
+	{
+		this.getAbstractRenderer().render(this._scene, this._camera);
 	}
 }
