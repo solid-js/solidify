7a8ae0dd05e2bc7cb9c39fe189f1dc2501bd3475
Alexis Bouhet <alexis@cher-ami.tv>
2018-02-08 11:44:27 +0100

    New react page implementation for playIn / playOut

--- react/ReactPage.ts
+++ react/ReactPage.ts
@@ -13,6 +13,14 @@ export interface ReactPageProps
 export class ReactPage<Props, States> extends ReactView<Props, States> implements IPage
 {
 	/**
+	 * Animating lock. Is true when animating with playIn or playOut.
+	 * Use this lock to disable features when animating.
+	 */
+	get animating ():boolean { return this._animating; }
+	protected _animating:boolean;
+	
+
+	/**
 	 * Constructor
 	 */
 	constructor (pProps:Props, pContext:any)
@@ -47,18 +55,72 @@ export class ReactPage<Props, States> extends ReactView<Props, States> implement
 	/**
 	 * Play intro animation.
 	 * Have to return a promise when animation is ended.
+	 * Animating lock is automatically enabled during animation.
+	 * Use animating lock to disable features when animating.
+	 * ex : if (this._animating) return
+	 * To implement animation, please override playInPromiseHandler.
 	 */
 	playIn ():Promise<any>
 	{
-		return new Promise( resolve => resolve() );
+		return new Promise( resolve =>
+		{
+			// Lock animating
+			this._animating = true;
+
+			// Execute promise
+			this.playInPromiseHandler( () =>
+			{
+				// Unlock animating
+				this._animating = false;
+
+				// Resolve promise
+				resolve();
+			});
+		});
 	}
 
 	/**
 	 * Play outro animation.
 	 * Have to return a promise when animation is ended.
+	 * Animating lock is automatically enabled during animation.
+	 * Use animating lock to disable features when animating.
+	 * ex : if (this._animating) return
+	 * To implement animation, please override playOutPromiseHandler.
 	 */
 	playOut ():Promise<any>
 	{
-		return new Promise( resolve => resolve() );
+		return new Promise( resolve =>
+		{
+			// Lock animating
+			this._animating = true;
+
+			this.playOutPromiseHandler( () =>
+			{
+				// Unlock animating
+				this._animating = false;
+
+				// Resolve promise
+				resolve();
+			});
+		});
+	}
+
+
+	/**
+	 * Override this method to implement play in animation.
+	 * Call complete handler when animation is done.
+	 */
+	protected playInPromiseHandler (pCompleteHandler:() => void)
+	{
+		pCompleteHandler();
+	}
+
+	/**
+	 * Override this method to implement play out animation.
+	 * Call complete handler when animation is done.
+	 */
+	protected playOutPromiseHandler (pCompleteHandler:() => void)
+	{
+		pCompleteHandler();
 	}
 }
